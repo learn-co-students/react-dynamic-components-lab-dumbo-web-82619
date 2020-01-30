6fad5513e3079c54903ac64af9544e1a815090e9
markdenardo <mark.denardo@gmail.com>
2020-01-30 12:01:18 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,10 +4,22 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{ opacity: this.props.opacity }}>
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
       </div>
     )
   }
 
 }
+
+// - Should expect a single prop (an opacity value), which can be used in the
+//   component via: `this.props.opacity`. This prop is first passed in `src/App.js`
+// - If the opacity value _is greater than or equal to 0.2_:
+//   - the `ColorBox` component should render another `ColorBox` itself (recursive
+//     components!)
+//   - an opacity prop should be passed to the child
+//   - the passed opacity prop should be reduced by 0.1
+// - If the opacity value _is less than 0.2_:
+//   - do not render another `ColorBox` (or else we would have infinite
+//     `ColorBoxes` rendering!)
+//   - instead, the render method should return `null`
